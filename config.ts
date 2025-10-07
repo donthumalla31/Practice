c92a6eda94cdcec83bf97957aea8e296dfc2f245
donthumalla31 <anil.donthumalla@ecodev.vic.gov.au>
2025-10-07 13:48:55 +1100

    config

---
+++ config.ts
@@ -0,0 +1,12 @@
+// Contains Configuration Constants
+
+const POWERAPPS_CONNECTORS = {
+    SharePoint: "shared_sharepointonline",
+    Office365: "office365",
+    Office365Users: "office365users",
+    Office365Groups: ""
+};
+
+const POWERAPPS_CONNECTIONS = {
+
+};
